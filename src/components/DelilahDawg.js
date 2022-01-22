7574c5832207d9c69d2306f2c3c65531c7d85958
Stephanie Hamilton <hamiltons621@gmail.com>
2022-01-22 12:24:26 -0600

    initial commit

--- src/components/DelilahDawg.js
+++ src/components/DelilahDawg.js
@@ -5,7 +5,7 @@ import { NavBar } from "./nav/NavBar"
 import { Login } from "./auth/Login"
 import { Register } from "./auth/Register"
 
-export const LevelUp = () => (
+export const DelilahDawg = () => (
     <>
         <Route render={() => {
             if (localStorage.getItem("lu_token")) {
